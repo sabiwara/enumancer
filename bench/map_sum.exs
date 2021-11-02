1359e978f06c112ab175f1bc4da8f169b4b3dc18
sabiwara <sabiwara@gmail.com>
2021-11-02 19:42:58 +0900

    Bench streams for big range

--- bench/map_sum.exs
+++ bench/map_sum.exs
@@ -1,18 +1,18 @@
-list = Enum.to_list(1..100)
+range = 1..1000_000
 
 defmodule Optimized do
   import Enumancer
 
-  defenum map_sum(list) do
-    list
+  defenum map_sum(range) do
+    range
     |> map(& &1 + 1)
     |> sum()
   end
 end
 
 Benchee.run(%{
-  "Enum.map/2 |> Enum.sum/1" => fn -> Enum.map(list, & &1 + 1) |> Enum.sum() end,
-  "Stream.map/2 |> Enum.sum/1" => fn -> Stream.map(list, & &1 + 1) |> Enum.sum() end,
-  "Enum.reduce/3" => fn -> Enum.reduce(list, 0, & &1 + &2 + 1) end,
-  "Enumancer map/2 |> sum/1" => fn -> Optimized.map_sum(list) end,
-})
+  "Enum.map/2 |> Enum.sum/1" => fn -> Enum.map(range, & &1 + 1) |> Enum.sum() end,
+  "Stream.map/2 |> Enum.sum/1" => fn -> Stream.map(range, & &1 + 1) |> Enum.sum() end,
+  "Enum.reduce/3" => fn -> Enum.reduce(range, 0, & &1 + &2 + 1) end,
+  "Enumancer map/2 |> sum/1" => fn -> Optimized.map_sum(range) end,
+}, memory_time: 1)
